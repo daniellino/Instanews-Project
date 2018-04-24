2ca983988a4a3630e127ec4120c7bc72152172d5
aroerick <a.roerick@gmail.com>
2018-04-24 16:09:34 -0700

    Add gulpfile(with browser-sync, uglify, eslint).

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("wat up boi")
+console.log("wat up boi");
+
+console.log("wat up boi");
+
+
+// General Kenobi
+
+console.log("wat up boi");
+
+console.log("holla back");
+        console.log("wat up boi");
