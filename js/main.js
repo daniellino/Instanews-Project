245be50a5d57850d1dd61eed1a540fe265159c49
aroerick <a.roerick@gmail.com>
2018-04-29 22:30:25 -0700

    Begin javascript API retrieval and list appends.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
-console.log("wat up boi");
+// Built by LucyBot. www.lucybot.com
+// var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+// url += "?" + $.param({
+//         "api-key": "63ff38af2226431eba347fc1bd5073e1"
+// });
 
-console.log("wat up boi");
+//EVERYTHING
+        $("#genre-select").on("change", function(){
+                $(".stories").empty();
 
+//Getting dat API right
+        var genreSelect = $(".genre").children(":selected").val();
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + genreSelect + ".json"; 
+        url+= "?" + $.param({
+                "api-key": "63ff38af2226431eba347fc1bd5073e1"
+        });
 
-// General Kenobi
+//      console.log(x);
+//      console.log(y);
 
-console.log("wat up boi");
+//Loopin for some data
+        $.ajax({
+                url: url,
+                method: "GET",
+        }).done(function(data) {
+                var a = data.results;
+                $.each( a, function( index, value ) {
+                        console.log(value);
 
-console.log("holla back");
-        console.log("wat up boi");
+                        var html = '';
+                        html += "<li class='list-item'>";
+                        html += "<a href='";
+                        html += value.url;
+                        html += "'>";
+                        html += value.abstract;
+                        html += "</a></li>";
+
+                        $(".stories").append(html);
+                });
+
+                // console.log(result);
+        }).fail(function(err) {
+                throw err;
+        });
+        });
